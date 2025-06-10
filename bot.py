513b8496f49995d6a8fb787591e105edc3fbeeeb
MAKSIM REZNICHENKO <maksimrez96@gmail.com>
2025-06-10 17:22:29 +0300

    Перенес на pyrotgfork. Добавил обработку forwarded и reply. Вынес проверки содержания в utilits. Добавил логи.

--- bot.py
+++ bot.py
@@ -1,56 +1,33 @@
-import os
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from decouple import config
 from pyrogram.types import Message
-import re
+from utilits import check_and_send_messages, check_command
 
 
+load_dotenv()
 api_id = config('API_ID')
 api_hash = config('API_HASH')
 phone = config('PHONE')
 login = config('LOGIN')
-load_dotenv()
 
 bot = Client(name=login, api_id=api_id, api_hash=api_hash, phone_number=phone)
 
-chat_id = os.getenv('CHAT_ID')
 
-@bot.on_message()
-async def echo_handler(client: Client, message: Message):
-    with open("keys.txt", "r", encoding="utf-8") as f:
-        content = f.read().strip()
-    words = [word.strip() for word in content.split(",") if word.strip()]
-    pattern = re.compile(r'(' + r'|'.join(words) + r')', flags=re.IGNORECASE)
+@bot.on_message(filters.channel)
+async def channel_handler(client: Client, message: Message):
+    await check_and_send_messages(client, message)
 
-    if message.chat.id == int(chat_id):
+@bot.on_message(filters.reply)
+async def reply_handler(client: Client, message: Message):
+    await check_and_send_messages(client, message)
 
-        if message.text == 'Ключи':
-            print(f'Текущий список ключей: {words}')
-            await client.send_message(chat_id=chat_id, text=f'Текущий список ключей: {words}')
+@bot.on_message(filters.forwarded)
+async def forwarded_handler(client: Client, message: Message):
+    await check_and_send_messages(client, message)
 
-        if re.compile(r"^Изменить:", flags=re.IGNORECASE).search(message.text):
-            parts = message.text.split(":", 1)
-            after_colon = parts[1].strip()
-
-            with open("keys.txt", "w", encoding="utf-8") as f:
-                f.write(after_colon)
-
-            print(f'Ключи изменены. Новый список: {after_colon}')
-            await client.send_message(chat_id=chat_id, text=f'Ключи изменены. Новый список: {after_colon}')
-
-        if re.compile(r"^Добавить:", flags=re.IGNORECASE).search(message.text):
-            parts = message.text.split(":", 1)
-            after_colon = parts[1].strip()
-            new_words = content + ', ' + after_colon
-            with open("keys.txt", "w", encoding="utf-8") as f:
-                f.write(new_words)
-            print(f'Ключи изменены. Новый список: {new_words}')
-            await client.send_message(chat_id=chat_id, text=f'Ключи изменены. Новый список: {new_words}')
-
-    if pattern.search(message.text):
-        await client.forward_messages(chat_id=chat_id, from_chat_id=message.chat.id, message_ids=message.id)
-
-
+@bot.on_message(filters.text)
+async def text_handler(client: Client, message: Message):
+    await check_command(client, message)
 
 bot.run()
---
+++ utilits.py
@@ -0,0 +1,72 @@
+import os
+import logging
+import colorlog
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
+chat_id = os.getenv('CHAT_ID')
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+    '%(log_color)s%(asctime)s - %(levelname)s - %(message)s',
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red',
+    }
+))
+
+logger = colorlog.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+async def check_and_send_messages(client, message):
+    with open("keys.txt", "r", encoding="utf-8") as f:
+        content = f.read().strip()
+    words = [word.strip() for word in content.split(",") if word.strip()]
+    pattern = re.compile(r'(' + r'|'.join(words) + r')', flags=re.IGNORECASE)
+
+    if message.text is not None:
+        logging.info(f'Проверка:\n```\n{message.text}\n```\n\n{pattern}\n\n{pattern.search(message.text)}')
+        if pattern.search(message.text):
+            await client.forward_messages(chat_id=chat_id, from_chat_id=message.chat.id, message_ids=message.id)
+
+    elif message.caption is not None:
+        logging.info(f'Проверка:\n```\n{message.caption}\n```\n\n{pattern}\n\n{pattern.search(message.caption)}')
+        if pattern.search(message.caption):
+            await client.forward_messages(chat_id=chat_id, from_chat_id=message.chat.id, message_ids=message.id)
+
+    elif message.text is None or message.caption is None:
+        logging.error(f'Не получилось получить текст! {message}')
+
+async def check_command(client, message):
+    with open("keys.txt", "r", encoding="utf-8") as f:
+        content = f.read().strip()
+    words = [word.strip() for word in content.split(",") if word.strip()]
+
+    if message.chat.id == int(chat_id):
+        if message.text == 'Ключи':
+            logging.info(f'Текущий список ключей: {words}')
+            await client.send_message(chat_id=chat_id, text=f'Текущий список ключей: {words}')
+
+        elif re.compile(r"^Изменить:", flags=re.IGNORECASE).search(message.text):
+            parts = message.text.split(":", 1)
+            after_colon = parts[1].strip()
+
+            with open("keys.txt", "w", encoding="utf-8") as f:
+                f.write(after_colon)
+
+            logging.info(f'Ключи изменены. Новый список: {after_colon}')
+            await client.send_message(chat_id=chat_id, text=f'Ключи изменены. Новый список: {after_colon}')
+
+        elif re.compile(r"^Добавить:", flags=re.IGNORECASE).search(message.text):
+            parts = message.text.split(":", 1)
+            after_colon = parts[1].strip()
+            new_words = content + ', ' + after_colon
+            with open("keys.txt", "w", encoding="utf-8") as f:
+                f.write(new_words)
+            logging.info(f'Ключи изменены. Новый список: {new_words}')
+            await client.send_message(chat_id=chat_id, text=f'Ключи изменены. Новый список: {new_words}')
